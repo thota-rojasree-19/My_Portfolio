d4ce76b93b69d48d1e4ce1de4341cecaff25bb01
thota-rojasree-68 <rojii3824@gmail.com>
2025-05-29 23:36:47 +0530

    Set up GitHub Pages deployment

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,22 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+
+
+
+
+
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/My_Portfolio/",  // 👈 Your repo name with slashes
   plugins: [react()],
 })
